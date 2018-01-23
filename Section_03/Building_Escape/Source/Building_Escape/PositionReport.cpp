ee2d1c1bd0fad8343b5f0985d2443fcab28237e4
Andrew Bresee <andrew.bresee@smartthings.com>
2018-01-23 08:37:30 -0800

    Add first logger and change player start

--- Section_03/Building_Escape/Source/Building_Escape/PositionReport.cpp
+++ Section_03/Building_Escape/Source/Building_Escape/PositionReport.cpp
@@ -1,6 +1,7 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Drew Bresee 2018
 
 #include "PositionReport.h"
+#include "Gameframework/Actor.h"
 
 
 // Sets default values for this component's properties
@@ -18,8 +19,7 @@ UPositionReport::UPositionReport()
 void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
+    UE_LOG(LogTemp, Warning, TEXT("Chair reporting for duty!"));
 	
 }
 
